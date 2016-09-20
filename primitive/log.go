00e6f0bcdc4c13abb813c87da62a756b8a3892d7
Michael Fogleman <fogleman@gmail.com>
2016-09-20 08:21:12 -0400

    log.go

---
+++ primitive/log.go
@@ -0,0 +1,23 @@
+package primitive
+
+import "fmt"
+
+var LogLevel int
+
+func Log(level int, format string, a ...interface{}) {
+	if LogLevel >= level {
+		fmt.Printf(format, a...)
+	}
+}
+
+func v(format string, a ...interface{}) {
+	Log(1, format, a...)
+}
+
+func vv(format string, a ...interface{}) {
+	Log(2, format, a...)
+}
+
+func vvv(format string, a ...interface{}) {
+	Log(3, format, a...)
+}
