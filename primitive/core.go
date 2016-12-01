11a9f18d04736b21486f5416ce16f37a9b4498ba
Zachary Wade <zacharywade@gmail.com>
2016-12-01 18:00:11 -0500

    Add support for transparency

--- primitive/core.go
+++ primitive/core.go
@@ -72,45 +72,53 @@ func differenceFull(a, b *image.RGBA) float64 {
 			ar := int(a.Pix[i])
 			ag := int(a.Pix[i+1])
 			ab := int(a.Pix[i+2])
+			aa := int(a.Pix[i+3])
 			br := int(b.Pix[i])
 			bg := int(b.Pix[i+1])
 			bb := int(b.Pix[i+2])
+			ba := int(b.Pix[i+3])
 			i += 4
 			dr := ar - br
 			dg := ag - bg
 			db := ab - bb
-			total += uint64(dr*dr + dg*dg + db*db)
+			da := aa - ba
+			total += uint64(dr*dr + dg*dg + db*db + da*da)
 		}
 	}
-	return math.Sqrt(float64(total)/float64(w*h*3)) / 255
+	return math.Sqrt(float64(total)/float64(w*h*4)) / 255
 }
 
 func differencePartial(target, before, after *image.RGBA, score float64, lines []Scanline) float64 {
 	size := target.Bounds().Size()
 	w, h := size.X, size.Y
-	total := uint64(math.Pow(score*255, 2) * float64(w*h*3))
+	total := uint64(math.Pow(score*255, 2) * float64(w*h*4))
 	for _, line := range lines {
 		i := target.PixOffset(line.X1, line.Y)
 		for x := line.X1; x <= line.X2; x++ {
 			tr := int(target.Pix[i])
 			tg := int(target.Pix[i+1])
 			tb := int(target.Pix[i+2])
+			ta := int(target.Pix[i+3])
 			br := int(before.Pix[i])
 			bg := int(before.Pix[i+1])
 			bb := int(before.Pix[i+2])
+			ba := int(before.Pix[i+3])
 			ar := int(after.Pix[i])
 			ag := int(after.Pix[i+1])
 			ab := int(after.Pix[i+2])
+			aa := int(after.Pix[i+3])
 			i += 4
 			dr1 := tr - br
 			dg1 := tg - bg
 			db1 := tb - bb
+			da1 := ta - ba
 			dr2 := tr - ar
 			dg2 := tg - ag
 			db2 := tb - ab
-			total -= uint64(dr1*dr1 + dg1*dg1 + db1*db1)
-			total += uint64(dr2*dr2 + dg2*dg2 + db2*db2)
+			da2 := ta - aa
+			total -= uint64(dr1*dr1 + dg1*dg1 + db1*db1 + da1*da1)
+			total += uint64(dr2*dr2 + dg2*dg2 + db2*db2 + da2*da2)
 		}
 	}
-	return math.Sqrt(float64(total)/float64(w*h*3)) / 255
+	return math.Sqrt(float64(total)/float64(w*h*4)) / 255
 }
