0592cfbd6ae3e0bb8f2a3cfe359e32fdde0d3b2b
NicholasChase <chase.22@wright.edu>
2020-03-24 17:45:35 -0400

    The beginning steps of the GUI has been created in GUI.py. This program pulls up the gui box. Also there is documentation in GUI.py

---
+++ GUI.py
@@ -0,0 +1,41 @@
+from tkinter import *
+
+def window():
+    root = Tk()
+    myLabel = Label(root, text="Primitive")
+    myLabel.pack()
+    root.mainloop()
+    
+window()
+
+
+#def window():
+#       Creates window for the program
+
+#TODO Req 1.0
+#def helpButton()
+#   Creates a help button
+
+#TODO 1.1
+#def export()
+#   allow the user to export their finished
+#   picture to the location of their choice 
+#TODO 1.2
+#def difficulty()
+#   allows the user to select the number of geometric shapes to form
+#   the image
+
+#TODO 1.3
+#def geometricShapes()
+#   allows the user to select what geometric shape to form
+#   the image
+
+#TODO 1.4
+#def display()
+#   Shows the final form of the image
+
+#TODO Req 1.5
+#def file()
+    #allows the user to select the photo they want
+    #import easygui
+    #file = easygui.fileopenbox()
